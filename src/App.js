d9ebd1b1d1e95299a6edac8f98753f8609daee2e
Tian Qiu <tian.qiu142@gmail.com>
2020-10-02 16:56:12 -0700

    added style including spinner from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Result from './Result';
 import axios from 'axios';
+import './style.css';
 
 class App extends React.Component {
-  state = { name1: '', name2: '', percentage: 0, sentence: '' };
+  state = {
+    name1: '',
+    name2: '',
+    percentage: 0,
+    sentence: '',
+    prediction: '',
+    loading: false,
+  };
 
   onInputChange1 = (event) => {
     this.setState({ name1: event.target.value });
@@ -11,10 +21,13 @@ class App extends React.Component {
   onInputChange2 = (event) => {
     this.setState({ name2: event.target.value });
   };
+  onPredictionChange = (event) => {
+    this.setState({ prediction: event.target.value });
+  };
 
-  onFormSubmit = (event) => {
+  onFormSubmit = async (event) => {
     event.preventDefault();
-    axios({
+    await axios({
       method: 'GET',
       url: 'https://love-calculator.p.rapidapi.com/getPercentage',
       headers: {
@@ -32,6 +45,7 @@ class App extends React.Component {
         this.setState({
           percentage: response.data.percentage,
           sentence: response.data.result,
+          loading: true,
         });
       })
       .catch((error) => {
@@ -41,24 +55,52 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.onFormSubmit}>
-          <input
-            type="text"
-            value={this.state.name1}
-            onChange={this.onInputChange1}
-          />
-          <input
-            type="text"
-            value={this.state.name2}
-            onChange={this.onInputChange2}
-          />
-          <input type="submit" />
-        </form>
-        <Result
-          percent={this.state.percentage}
-          sentence={this.state.sentence}
-        />
+      <div className="main">
+        <div className="left-side">
+          <div id="title">Love Calculator</div>
+          <div id="instruction">Start by entering two names</div>
+          <hr />
+          <form onSubmit={this.onFormSubmit}>
+            <input
+              className="input"
+              type="text"
+              value={this.state.name1}
+              onChange={this.onInputChange1}
+              placeholder="Name one"
+            />
+            <input
+              className="input"
+              type="text"
+              value={this.state.name2}
+              onChange={this.onInputChange2}
+              placeholder="Name two"
+            />
+            <input
+              className="input"
+              type="text"
+              value={this.state.prediction}
+              onChange={this.onPredictionChange}
+              placeholder="Prediction (1-100)"
+            />
+            <button class="submit" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className="right-side-main">
+          {this.state.loading ? (
+            <Result
+              percent={this.state.percentage}
+              sentence={this.state.sentence}
+              prediction={this.state.prediction}
+              name1={this.state.name1}
+              name2={this.state.name2}
+            />
+          ) : (
+            <Spinner animation="border" />
+          )}
+        </div>
       </div>
     );
   }
--- src/Result.js
+++ src/Result.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './style.css';
 
-const Result = ({ percent, sentence }) => {
+const Result = ({ percent, sentence, prediction, name1, name2 }) => {
+  const judgePrediction = () => {
+    if (!sentence) {
+      return;
+    } else if (prediction > percent && prediction - percent < 25) {
+      return 'Not as compatable as you have hoped, but close!';
+    } else if (prediction > percent && prediction - percent > 25) {
+      return 'You had high hopes, but reality can be cruel sometimes.';
+    } else if (prediction < percent && percent - prediction > 25) {
+      return 'You are in for a plesant suprise!';
+    } else if (prediction < percent && percent - prediction < 25) {
+      return 'Pretty close! You both are more compatable than you think!';
+    } else if (prediction === percent) {
+      return 'Nice Guess, right on the target!';
+    }
+  };
+
+  const aiScore = () => {
+    return `${name1} and ${name2}'s Match Score is: ${percent}%`;
+  };
+
   return (
-    <div>
-      <div>Match Score: {percent}%</div>
-      <div>The Love AI says: {sentence}</div>
+    <div className="right-side">
+      <div className="result" id="first-result">
+        You Predicted: {prediction}%
+      </div>
+      <div className="result">{aiScore()}</div>
+      <div className="result">{judgePrediction()}</div>
+      <div className="result">The Love AI says: "{sentence}."</div>
     </div>
   );
 };
